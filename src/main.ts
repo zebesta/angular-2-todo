caaeba34cdf9c11a77b9ae31b7004664888b9891
zebesta <chris.sebesta@gmail.com>
2016-09-02 15:43:34 -0400

    fixed boot strapping

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
+import { environment } from './app/';
+import { TodoAppComponent } from './app/todo-app';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(TodoAppComponent);
